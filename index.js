2d529e4337e2a2a4d0ba1a69d62c3719ad8e11ee
Derek Miranda <derek.miranda@derekmi-macpro.ff0000.com>
2017-12-06 18:03:23 -0800

    Color and Object Utils

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
-import ColorUtils from './lib/ColorUtils'
 import DcsUtils from './lib/DcsUtils'
 import ImageUtils from './lib/ImageUtils'
 import LocationUtils from './lib/LocationUtils'
 // import MonetUtils from './lib/MonetUtils'
 // import MotionUtils from './lib/MotionUtils'
-import ObjectUtils from './lib/ObjectUtils'
 import TextUtils from './lib/TextUtils'
 import NetUtils from './lib/NetUtils'
 
 // has named exports
+import * as ObjectUtils from './lib/ObjectUtils'
+import * as ColorUtils from './lib/ColorUtils'
 import * as ArrayUtils from './lib/ArrayUtils'
 import * as MathUtils from './lib/MathUtils'
+
+console.log('keys', Object.keys(ObjectUtils))
 
 export {
 	ArrayUtils,
