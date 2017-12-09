5f29dca5e0d91b0ec08effedf403f4b534f6278c
Derek Miranda <derek.miranda@ff0000.com>
2017-12-08 16:10:25 -0800

    ImageUtils

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 
 import DcsUtils from './lib/DcsUtils'
-import ImageUtils from './lib/ImageUtils'
 import LocationUtils from './lib/LocationUtils'
 // import MonetUtils from './lib/MonetUtils'
 // import MotionUtils from './lib/MotionUtils'
 import TextUtils from './lib/TextUtils'
 import NetUtils from './lib/NetUtils'
 
-// has named exports
-
-// NOTE: DON'T import only "default" from ObjectUtils since the default
-// function is a reserved keyword. Importing into scope will likely cause
-// issues w/ code using the "default" keyword
+/* NOTE: DON'T import only "default" from ObjectUtils since the default
+function is a reserved keyword. Importing into scope will likely cause
+issues w/ code using the "default" keyword */
 import * as ObjectUtils from './lib/ObjectUtils'
 import * as ColorUtils from './lib/ColorUtils'
 import * as ArrayUtils from './lib/ArrayUtils'
 import * as MathUtils from './lib/MathUtils'
+import * as ImageUtils from './lib/ImageUtils'
 
 console.log('keys', Object.keys(ObjectUtils))
 
