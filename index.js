cf897fcd55940ad69d1586f13f8e2882fef75cd0
Derek Miranda <derek.miranda@ff0000.com>
2017-12-15 09:19:46 -0800

    TEMP: disabling non-exported from modules

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ export { default as NetUtils } from './lib/NetUtils'
 /* NOTE: DON'T import only "default" from ObjectUtils since the default
 function is a reserved keyword. Importing into scope will likely cause
 issues w/ code using the "default" keyword */
-import * as ObjectUtils from './lib/ObjectUtils'
-import * as ColorUtils from './lib/ColorUtils'
-import * as ArrayUtils from './lib/ArrayUtils'
-import * as MathUtils from './lib/MathUtils'
-import * as ImageUtils from './lib/ImageUtils'
+// import * as ObjectUtils from './lib/ObjectUtils'
+// import * as ColorUtils from './lib/ColorUtils'
+// import * as ArrayUtils from './lib/ArrayUtils'
+// import * as MathUtils from './lib/MathUtils'
+// import * as ImageUtils from './lib/ImageUtils'
 
-export {
-	ArrayUtils,
-	ColorUtils,
-	DcsUtils,
-	ImageUtils,
-	LocationUtils,
-	MathUtils,
-	MonetUtils,
-	MotionUtils,
-	ObjectUtils,
-	TextUtils,
-	NetUtils,
-}
+// export {
+// 	ArrayUtils,
+// 	ColorUtils,
+// 	DcsUtils,
+// 	ImageUtils,
+// 	LocationUtils,
+// 	MathUtils,
+// 	MonetUtils,
+// 	MotionUtils,
+// 	ObjectUtils,
+// 	TextUtils,
+// 	NetUtils,
+// }
