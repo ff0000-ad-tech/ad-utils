f8927cae0874eb9f0ec40c49968dd865ad5db417
Derek Miranda <derek.miranda@ff0000.com>
2017-12-15 11:57:17 -0800

    Reusing import *

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ export { default as MotionUtils } from './lib/MotionUtils'
 export { default as TextUtils } from './lib/TextUtils'
 export { default as NetUtils } from './lib/NetUtils'
 
-// using a unifying module for modules w/ named exports
-export { default as MathUtils } from './lib/MathUtils.all'
-
 /* NOTE: DON'T import only "default" from ObjectUtils since the default
 function is a reserved keyword. Importing into scope will likely cause
 issues w/ code using the "default" keyword */
-// import MathUtils from './lib/MathUtils.all'
 import * as ObjectUtils from './lib/ObjectUtils'
+import * as MathUtils from './lib/MathUtils'
 import * as ColorUtils from './lib/ColorUtils'
 import * as ArrayUtils from './lib/ArrayUtils'
 import * as ImageUtils from './lib/ImageUtils'
 
 export {
+	MathUtils,
 	ArrayUtils,
 	ColorUtils,
 	ImageUtils,
