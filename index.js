9f57d0471e0d17919bb57c243b0a4c53f7f18a59
Derek Miranda <derek.miranda@ff0000.com>
2017-12-15 09:24:32 -0800

    Trying unifying module pattern on MathUtils

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ export { default as MotionUtils } from './lib/MotionUtils'
 export { default as TextUtils } from './lib/TextUtils'
 export { default as NetUtils } from './lib/NetUtils'
 
+// using a unifying module for modules w/ named exports
+export { default as MathUtils } from './lib/MathUtils.all'
+
 /* NOTE: DON'T import only "default" from ObjectUtils since the default
 function is a reserved keyword. Importing into scope will likely cause
 issues w/ code using the "default" keyword */
 // import * as ObjectUtils from './lib/ObjectUtils'
 // import * as ColorUtils from './lib/ColorUtils'
 // import * as ArrayUtils from './lib/ArrayUtils'
-// import * as MathUtils from './lib/MathUtils'
 // import * as ImageUtils from './lib/ImageUtils'
 
 // export {
