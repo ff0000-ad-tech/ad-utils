3cc420b7aca1364174f8346a8c46fb2ed3cafcb0
Derek Miranda <derek.miranda@ff0000.com>
2017-12-15 10:40:31 -0800

    Now test MathUtils w/ all module

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ export { default as TextUtils } from './lib/TextUtils'
 export { default as NetUtils } from './lib/NetUtils'
 
 // using a unifying module for modules w/ named exports
-// export { default as MathUtils } from './lib/MathUtils.all'
+export { default as MathUtils } from './lib/MathUtils.all'
 
 /* NOTE: DON'T import only "default" from ObjectUtils since the default
 function is a reserved keyword. Importing into scope will likely cause
 issues w/ code using the "default" keyword */
-import * as MathUtils from './lib/MathUtils.all'
+// import MathUtils from './lib/MathUtils.all'
 import * as ObjectUtils from './lib/ObjectUtils'
 import * as ColorUtils from './lib/ColorUtils'
 import * as ArrayUtils from './lib/ArrayUtils'
