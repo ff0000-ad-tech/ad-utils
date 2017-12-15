2c3483c33f671503a1109ff102d5df3d6cbe1a1c
Derek Miranda <derek.miranda@ff0000.com>
2017-12-15 10:38:41 -0800

    Back to * imports

--- index.js
+++ index.js
@@ -7,26 +7,21 @@ export { default as TextUtils } from './lib/TextUtils'
 export { default as NetUtils } from './lib/NetUtils'
 
 // using a unifying module for modules w/ named exports
-export { default as MathUtils } from './lib/MathUtils.all'
+// export { default as MathUtils } from './lib/MathUtils.all'
 
 /* NOTE: DON'T import only "default" from ObjectUtils since the default
 function is a reserved keyword. Importing into scope will likely cause
 issues w/ code using the "default" keyword */
-// import * as ObjectUtils from './lib/ObjectUtils'
-// import * as ColorUtils from './lib/ColorUtils'
-// import * as ArrayUtils from './lib/ArrayUtils'
-// import * as ImageUtils from './lib/ImageUtils'
+import * as MathUtils from './lib/MathUtils.all'
+import * as ObjectUtils from './lib/ObjectUtils'
+import * as ColorUtils from './lib/ColorUtils'
+import * as ArrayUtils from './lib/ArrayUtils'
+import * as ImageUtils from './lib/ImageUtils'
 
-// export {
-// 	ArrayUtils,
-// 	ColorUtils,
-// 	DcsUtils,
-// 	ImageUtils,
-// 	LocationUtils,
-// 	MathUtils,
-// 	MonetUtils,
-// 	MotionUtils,
-// 	ObjectUtils,
-// 	TextUtils,
-// 	NetUtils,
-// }
+export {
+	ArrayUtils,
+	ColorUtils,
+	ImageUtils,
+	MathUtils,
+	ObjectUtils,
+}
