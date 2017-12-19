47611328b40ace191e9380c00d1b9f0dff9e5b8a
Derek Miranda <derek.miranda@ff0000.com>
2017-12-19 11:57:48 -0800

    Named Dcs exports

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-export { default as DcsUtils } from './lib/DcsUtils'
 export { default as LocationUtils } from './lib/LocationUtils'
 export { default as MotionUtils } from './lib/MotionUtils'
 export { default as TextUtils } from './lib/TextUtils'
@@ -8,6 +7,7 @@ export { default as NetUtils } from './lib/NetUtils'
 /* NOTE: DON'T import only "default" from ObjectUtils since the default
 function is a reserved keyword. Importing into scope will likely cause
 issues w/ code using the "default" keyword */
+import * as DcsUtils from './lib/DcsUtils'
 import * as ObjectUtils from './lib/ObjectUtils'
 import * as MathUtils from './lib/MathUtils'
 import * as ColorUtils from './lib/ColorUtils'
@@ -15,6 +15,7 @@ import * as ArrayUtils from './lib/ArrayUtils'
 import * as ImageUtils from './lib/ImageUtils'
 
 export {
+	DcsUtils,
 	MathUtils,
 	ArrayUtils,
 	ColorUtils,
