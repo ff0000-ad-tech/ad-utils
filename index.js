a67068fa7c17c8abfe54501910120102e6c9f1c5
Kenny Arehart <kenny.arehart@ff0000.com>
2018-06-05 09:57:46 -0700

    Removed duplicate NetUtils method, cleaned up other classes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import { default as MonetUtils } from './lib/MonetUtils'
 /* NOTE: DON'T import only "default" from ObjectUtils since the default
 function is a reserved keyword. Importing into scope will likely cause
 issues w/ code using the "default" keyword */
-import * as DcsUtils from './lib/DcsUtils'
 import * as ObjectUtils from './lib/ObjectUtils'
+import * as DcsUtils from './lib/DcsUtils'
 import * as MathUtils from './lib/MathUtils'
 import * as ColorUtils from './lib/ColorUtils'
 import * as ArrayUtils from './lib/ArrayUtils'
@@ -16,4 +16,4 @@ import * as NetUtils from './lib/NetUtils'
 
 export { DcsUtils, MathUtils, ArrayUtils, ColorUtils, ImageUtils, ObjectUtils, MonetUtils, NetUtils, TextUtils }
 
-export { default as getQueryParams } from './lib/getQueryParams.js'
+export { default as getQueryParam } from './lib/getQueryParam.js'
