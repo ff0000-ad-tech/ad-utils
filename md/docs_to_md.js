0ee961da7d3ab0d9dc39911794f145923c78872f
Greg Connell <gconnell@160over90.com>
2021-09-07 15:54:13 -0700

    updates imports and tosses auto-docs  - most of this pkg is deprecated

--- md/docs_to_md.js
+++
@@ -1,2 +0,0 @@
-const fatJsdoc2md = require('@ff0000-ad-tech/ad-docs')
-fatJsdoc2md.createMarkdownFromClasses('ad-utils', `${__dirname}/../`, `${__dirname}/api.hbs`, `${__dirname}/README.hbs`)
--- test/testHelper.js
+++
@@ -1,7 +0,0 @@
-require('babel-core/register')()
-require('jsdom-global/register')
-
-// mocking trace
-global.trace = function(...args) {
-	if (process.env.TRACE) console.log(...args)
-}
